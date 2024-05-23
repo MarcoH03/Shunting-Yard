6753e7113fb47a679a6b2160c9f5066888078a9e
MarcoH03 <marcocanizarestan0331@gmail.com>
2024-05-23 02:28:12 -0400

    ya puede Calc tda cmbncion de fn y num. Falta leer txt

--- funciones.py
+++ funciones.py
@@ -11,35 +11,35 @@ def shunting_yard(expression):
 
     precedence={'+':1,'-':1,'*':2,'/':2}
     
-    expression = re.findall(r"[+\-*/()\[\]]|\d*\.\d+|\d+|\w+", expression)
+    expression = re.findall(r"[+\-*/()\[\]]|\d*\.\d+|\d+|\w+|\,", expression)
 
     for token in enumerate(expression):
         
         if token[1].isalpha(): #es un nombre de funcion
             if token[1] in functions:
-                argumentos = [expression[token[0]+j+2] for j in range(functions[token[1]]['args'])]
+                argumentos = [expression[token[0]+j*2+2] for j in range(functions[token[1]]['args'])]
                 output_queue.append(evaluate_function(token[1], *argumentos))
             else:
                 print(f"La función {token[1]} no está definida.")
                 proxima()
                 
-        elif token[1].isnumeric():
+        elif token[1].isnumeric() and expression[token[0]-1] != ',' and expression[token[0]-1] != '[':
             output_queue.append(float(token[1]))
 
-        elif token in precedence:
+        elif token[1] in precedence:
 
             while (operator_stack and not operator_stack[-1]=='(' 
-                and precedence[token]<=precedence[operator_stack[-1]]):
+                and precedence[token[1]]<=precedence[operator_stack[-1]]):
 
                 output_queue.append(operator_stack.pop())
 
-            operator_stack.append(token)
+            operator_stack.append(token[1])
 
-        elif token=='(':
+        elif token[1]=='(':
 
-            operator_stack.append(token)
+            operator_stack.append(token[1])
 
-        elif token==')':
+        elif token[1]==')':
 
             while operator_stack and not operator_stack[-1]=='(':
 
@@ -50,8 +50,48 @@ def shunting_yard(expression):
     while operator_stack:
 
         output_queue.append(operator_stack.pop())
-
-    return output_queue
+        
+    while len(output_queue) != 1:
+        size = len(output_queue)
+        i=0
+        for j in range(size):
+            if output_queue[i] in precedence:
+                if output_queue[i]=='+':
+                    a = output_queue[i-2]
+                    b = output_queue[i-1]
+                    output_queue[i-2] = a+b
+                    i=i-2
+                    output_queue.pop(i+1)                        
+                    output_queue.pop(i+1)
+                elif output_queue[i]=='-':
+                    a = output_queue[i-2]
+                    b = output_queue[i-1]
+                    output_queue[i-2] = a-b
+                    i=i-2
+                    output_queue.pop(i+1)                        
+                    output_queue.pop(i+1)
+                elif output_queue[i]=='*':
+                    a = output_queue[i-2]
+                    b = output_queue[i-1]
+                    output_queue[i-2] = a*b
+                    i=i-2
+                    output_queue.pop(i+1)                        
+                    output_queue.pop(i+1)
+                elif output_queue[i]=='/':
+                    a = output_queue[i-2]
+                    b = output_queue[i-1]
+                    
+                    if b==0:
+                        print("Division por cero")
+                        proxima()
+                    else:
+                        output_queue[i-2] = a/b
+                        i=i-2
+                        output_queue.pop(i+1)                        
+                        output_queue.pop(i+1)
+            i=i+1
+    return output_queue[0]  
+                    
 
 def add_function(function_str,functions):
     
@@ -88,13 +128,52 @@ def add_function(function_str,functions):
     functions[func_name]={'args':num_params,'params':params,'expression':expression}
 
 def evaluate_function(func_name,*args):
-    pass
-    # if func_name not in functions:
-    #     print(f"La función {func_name} no está definida.")
-    #     proxima()
-    # if not len(args)==functions[func_name]['args']:
-    #     print(f"La función {func_name} requiere {functions[func_name]['args']} argumentos.")
-    #     proxima()
-    # print(f"La función {func_name} con argumentos {args} es igual a {evaluate_expression(functions[func_name]['expression'],dict(zip(functions[func_name]['params'],args)))}")
+    if func_name not in functions:
+        print(f"La función {func_name} no está definida.")
+        proxima()
+    if not len(args)==functions[func_name]['args']:
+        print(f"La función {func_name} requiere {functions[func_name]['args']} argumentos.")
+        proxima()
+    #print(f"La función {func_name} con argumentos {args} es igual a {evaluate_expression(functions[func_name]['expression'],dict(zip(functions[func_name]['params'],args)))}")
+    
+    expression = functions[func_name]['expression']
+    for i in range(len(args)):
+        expression = expression.replace(functions[func_name]['params'][i],args[i])
+        
+    return shunting_yard(expression) 
+    
+functions={}
 
-functions = {}
+def main():
+
+    while True:
+
+        print("\n1. Añadir nueva función\n3. Evaluar expresión\n4. Salir")
+
+        choice=input("Selecciona una opción: ")
+
+        if choice=="1":
+
+            function_str=input("Introduce la nueva función: ")
+
+            add_function(function_str,functions)
+
+        elif choice=="3":
+
+            expression=input("Introduce la expresión a evaluar: ")
+
+            result=shunting_yard(expression)
+
+            print("Resultado:",result)
+
+        elif choice=="4":
+
+            break
+
+        else:
+
+            print("Opción no válida. Inténtalo de nuevo.")
+
+if __name__ == "__main__":
+
+    main()
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import re
 
-expression = "f[3] + 3 +3.5 / (4+5) "
 
-print(list(re.findall(r"[+\-*/()\[\]]|\d*\.\d+|\d+|\w+", expression)))
+
